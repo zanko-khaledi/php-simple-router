56b7960fe477f740886d0095ef152d2d1ae01893
zanko-khaledi <zanko.kh75@gmail.com>
2023-04-26 01:48:07 +0430

    refactore.

--- src/Abstracts/BaseRoute.php
+++ src/Abstracts/BaseRoute.php
@@ -8,24 +8,39 @@ abstract class BaseRoute
     protected static ?array $routes = [];
     protected ?string $name = null;
 
+    /**
+     * @param string|null $name
+     * @param string $path
+     * @return void
+     */
     public function setRoute(?string $name, string $path)
     {
         static::$routes[$name] = $path;
     }
 
+    /**
+     * @return array|null
+     */
     public static function getRoutes(): ?array
     {
         return static::$routes;
     }
 
+    /**
+     * @param string $name
+     * @return string|null
+     */
     public static function route(string $name): ?string
     {
         return static::$routes[$name];
     }
 
+    /**
+     * @return void
+     */
     public static function list()
     {
-        foreach (static::getRoutes() as $name => $route){
+        foreach (static::getRoutes() as $name => $route) {
             echo "$name --------------> $route <br>";
         }
     }
