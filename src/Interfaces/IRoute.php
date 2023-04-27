9b497ab44dbf0b35546103106ec647ad078fa3d4
zanko-khaledi <zanko.kh75@gmail.com>
2023-04-28 01:35:36 +0430

    Router.php was refactored.

--- src/Interfaces/IRoute.php
+++ src/Interfaces/IRoute.php
@@ -4,5 +4,11 @@ namespace ZankoKhaledi\PhpSimpleRouter\Interfaces;
 
 interface IRoute
 {
-    public function addRoute(string $method,string $path,callable|array $callback,string $pattern = null);
+    public function addRoute(string $method,string $path,callable|array $callback):IRoute;
+
+    public function where(string $pattern):IRoute;
+
+    public function name(string $name):IRoute;
+
+    public function serve();
 }
