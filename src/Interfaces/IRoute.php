8daee1d989b6fd017f22c5dd1325305eb02927c3
zanko-khaledi <zanko.kh75@gmail.com>
2023-05-04 23:50:56 +0430

    Router.php was refactored.

--- src/Interfaces/IRoute.php
+++ src/Interfaces/IRoute.php
@@ -6,9 +6,9 @@ interface IRoute
 {
     public function addRoute(string $method,string $path,callable|array $callback):IRoute;
 
-    public function where(string $pattern):IRoute;
-
     public function middleware(array $middlewares):IRoute;
+
+    public function where(string $pattern):IRoute;
 
     public function serve();
 }
